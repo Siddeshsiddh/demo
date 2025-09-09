7e2d32c2a879e38be5b930d4e0d5d00b4f13ed08
Siddesh <siddeshpgowda@gmail.com>
2025-09-09 17:08:43 +0530

    new one

--- app.py
+++ app.py
@@ -19,8 +19,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    img = Image.new('RGB', (60, 30), color = 'red')
-    return "Image created!"
+    # img = Image.new('RGB', (60, 30), color = 'red')
+    # return "Image created!"
+    return "hi siddesh how r u"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')

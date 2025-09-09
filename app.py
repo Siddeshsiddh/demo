133420d8033b13ab42c337cc39120eb27b45e47c
Siddesh <siddeshpgowda@gmail.com>
2025-09-09 17:34:31 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -1,16 +1,3 @@
-# from flask import Flask
-
-# app = Flask(__name__)
-
-# @app.route('/')
-# def home():
-#     return "Hello Siddesh! 🚀 Your Flask app is running inside Docker!"
-
-# if __name__ == "__main__":
-#     app.run(host='0.0.0.0', port=5000,debug=True)
-
-
-# print("hi i am siddesh")
 
 from flask import Flask
 from PIL import Image
